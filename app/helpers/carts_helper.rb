d0f7b93f2d740f961aff627b590acecd15b01513
Luat Hoang <luat1203@gmail.com>
2019-04-09 09:56:36 +0700

    Task# Create, show, index, update, delete  order

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -1,9 +1,6 @@
 module CartsHelper
   def find_product id
-    product = Product.find_by id: id
-    return product if product
-    flash[:danger] = t "controllers.carts.product_not_found"
-    redirect_to products_path
+    Product.find_by id: id
   end
 
   def cart_products
@@ -16,12 +13,10 @@ module CartsHelper
     cart_products.blank? ? Settings.carts.no_product : cart_products.size
   end
 
-  def calculate_total_price cart_products
-    @total = 0
-    cart_products.each do |id, quantity|
-      product = Product.find_by id: id
-      @total += product.price.to_i * quantity.to_i if product.present?
+  def calculate_total_price
+    cart_products.sum do |id, quantity|
+      product = find_product id
+      product.present? ? product.price.to_i * quantity.to_i : 0
     end
-    @total
   end
 end
